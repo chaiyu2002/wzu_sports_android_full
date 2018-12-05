314195756d65e710b36bb5f3cf84810846a07d30
xuke <blk1314@126.com>
2018-12-05 14:18:14 +0800

    增加开屏广告

--- UniversitySport/app/src/main/java/com/tim/app/ui/activity/SplashActivity.java
+++ UniversitySport/app/src/main/java/com/tim/app/ui/activity/SplashActivity.java
@@ -1,22 +1,31 @@
 package com.tim.app.ui.activity;
 
 import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.util.Base64;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.application.library.log.DLOG;
 import com.lzy.okhttputils.OkHttpUtils;
 import com.tim.app.R;
+import com.tim.app.constant.ADConstant;
 import com.tim.app.constant.AppStatusConstant;
 import com.tim.app.constant.AppStatusManager;
 import com.tim.app.server.entry.User;
@@ -26,11 +35,18 @@ import com.tim.app.util.ViewGT;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.qq.e.ads.splash.SplashAD;
+import com.qq.e.ads.splash.SplashADListener;
+import com.qq.e.comm.util.AdError;
 
 import static com.tim.app.constant.AppConstant.student;
 import static com.tim.app.constant.AppConstant.user;
 
-public class SplashActivity extends BaseActivity {
+
+public class SplashActivity extends BaseActivity implements SplashADListener{
 
     private static final String TAG = "SplashActivity";
 
@@ -40,10 +56,76 @@ public class SplashActivity extends BaseActivity {
 
     private Button btLogin;
 
+    private SplashAD splashAD;
+    private ViewGroup container;
+    private TextView skipView;
+    private ImageView splashHolder;
+    private static final String SKIP_TEXT = "点击跳过 %d";
+    public boolean canJump = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         AppStatusManager.getInstance().setAppStatus(AppStatusConstant.STATUS_NORMAL); //进入应用初始化设置成未登录状态
         super.onCreate(savedInstanceState);
+    }
+
+    /**
+     *
+     * ----------非常重要----------
+     *
+     * Android6.0以上的权限适配简单示例：
+     *
+     * 如果targetSDKVersion >= 23，那么必须要申请到所需要的权限，再调用广点通SDK，否则广点通SDK不会工作。
+     *
+     * Demo代码里是一个基本的权限申请示例，请开发者根据自己的场景合理地编写这部分代码来实现权限申请。
+     * 注意：下面的`checkSelfPermission`和`requestPermissions`方法都是在Android6.0的SDK中增加的API，如果您的App还没有适配到Android6.0以上，则不需要调用这些方法，直接调用广点通SDK即可。
+     */
+    @TargetApi(Build.VERSION_CODES.M)
+    private void checkAndRequestPermission() {
+        List<String> lackedPermission = new ArrayList<String>();
+        if (!(checkSelfPermission(Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED)) {
+            lackedPermission.add(Manifest.permission.READ_PHONE_STATE);
+        }
+
+        if (!(checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
+            lackedPermission.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)) {
+            lackedPermission.add(Manifest.permission.ACCESS_FINE_LOCATION);
+        }
+
+        if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE);
+        } else {
+            UserManager.instance().cleanCache();
+        }
+
+        // 权限都已经有了，那么直接调用SDK
+        if (lackedPermission.size() == 0) {
+            fetchSplashAD(this, container, null, ADConstant.APPID, ADConstant.SplashPosID, this, 0);
+        } else {
+            // 请求所缺少的权限，在onRequestPermissionsResult中再看是否获得权限，如果获得权限就可以调用SDK，否则不要调用SDK。
+            String[] requestPermissions = new String[lackedPermission.size()];
+            lackedPermission.toArray(requestPermissions);
+            requestPermissions(requestPermissions, 1024);
+        }
+    }
+
+    /**
+     * 拉取开屏广告，开屏广告的构造方法有3种，详细说明请参考开发者文档。
+     *
+     * @param activity        展示广告的activity
+     * @param adContainer     展示广告的大容器
+     * @param skipContainer   自定义的跳过按钮：传入该view给SDK后，SDK会自动给它绑定点击跳过事件。SkipView的样式可以由开发者自由定制，其尺寸限制请参考activity_splash.xml或者接入文档中的说明。
+     * @param appId           应用ID
+     * @param posId           广告位ID
+     * @param adListener      广告状态监听器
+     * @param fetchDelay      拉取广告的超时时长：取值范围[3000, 5000]，设为0表示使用广点通SDK默认的超时时长。
+     */
+    private void fetchSplashAD(Activity activity, ViewGroup adContainer, View skipContainer,
+                               String appId, String posId, SplashADListener adListener, int fetchDelay) {
+        splashAD = new SplashAD(activity, adContainer, skipContainer, appId, posId, adListener, fetchDelay);
     }
 
     @Override
@@ -60,25 +142,19 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     public void initView() {
-        btLogin = (Button) findViewById(R.id.btLogin);
-        btLogin.setOnClickListener(this);
+        container = (ViewGroup) findViewById(R.id.splash_container);
+        skipView = (TextView) findViewById(R.id.skip_view);
+        splashHolder = (ImageView) findViewById(R.id.splash_holder);
     }
 
     @Override
     public void initData() {
-
-        if (!(ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_PERMISSION_WRITE_EXTERNAL_STORAGE);
+        // 如果targetSDKVersion >= 23，就要申请好权限。如果您的App没有适配到Android6.0（即targetSDKVersion < 23），那么只需要在这里直接调用fetchSplashAD接口。
+        if (Build.VERSION.SDK_INT >= 23) {
+            checkAndRequestPermission();
         } else {
-            UserManager.instance().cleanCache();
-        }
-        user = getUserFromCache();
-        if (user != null) {
-            showLoadingDialog();
-
-            student = user.getStudent();
-            MainActivity.start(this);
-            finish();
+            // 如果是Android6.0以下的机器，默认在安装时获得了所有权限，可以直接调用SDK
+            fetchSplashAD(this, container, skipView, ADConstant.APPID, ADConstant.SplashPosID, this, 0);
         }
     }
 
@@ -117,12 +193,6 @@ public class SplashActivity extends BaseActivity {
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()) {
-            case R.id.btLogin:
-                ViewGT.gotoLoginActivity(this);
-                break;
-        }
-
     }
 
     Handler mHandler = new Handler() {
@@ -149,8 +219,17 @@ public class SplashActivity extends BaseActivity {
         super.onResume();
         handleIntent();
         hideLoadingDialog();
+        if (canJump) {
+            next();
+        }
+        canJump = true;
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        canJump = false;
+    }
 
     @Override
     protected void onDestroy() {
@@ -166,5 +245,78 @@ public class SplashActivity extends BaseActivity {
     protected void onStop() {
         super.onStop();
         finish();
+    }
+
+    @Override
+    public void onNoAD(AdError error) {
+        Log.i(
+                "AD_DEMO",
+                String.format("LoadSplashADFail, eCode=%d, errorMsg=%s", error.getErrorCode(),
+                        error.getErrorMsg()));
+        /** 如果加载广告失败，则直接跳转 */
+//        this.startActivity(new Intent(this, MainActivity.class));
+        navigateToNext();
+        this.finish();
+    }
+    @Override
+    public void onADPresent() {
+        Log.i("AD_DEMO", "SplashADPresent");
+        splashHolder.setVisibility(View.INVISIBLE); // 广告展示后一定要把预设的开屏图片隐藏起来
+    }
+    @Override
+    public void onADClicked() {
+        Log.i("AD_DEMO", "SplashADClicked");
+    }
+
+    /**
+     * 倒计时回调，返回广告还将被展示的剩余时间。
+     * 通过这个接口，开发者可以自行决定是否显示倒计时提示，或者还剩几秒的时候显示倒计时
+     *
+     * @param millisUntilFinished 剩余毫秒数
+     */
+    @Override
+    public void onADTick(long millisUntilFinished) {
+        Log.i("AD_DEMO", "SplashADTick " + millisUntilFinished + "ms");
+        skipView.setText(String.format(SKIP_TEXT, Math.round(millisUntilFinished / 1000f)));
+    }
+
+    @Override
+    public void onADDismissed() {
+        Log.i("AD_DEMO", "SplashADDismissed");
+        next();
+    }
+    /**
+     * 设置一个变量来控制当前开屏页面是否可以跳转，当开屏广告为普链类广告时，点击会打开一个广告落地页，此时开发者还不能打开自己的App主页。当从广告落地页返回以后，
+     * 才可以跳转到开发者自己的App主页；当开屏广告是App类广告时只会下载App。
+     */
+    private void next() {
+        if (canJump) {
+//            this.startActivity(new Intent(this, MainActivity.class));
+            navigateToNext();
+            this.finish();
+        } else {
+            canJump = true;
+        }
+    }
+    /** 开屏页一定要禁止用户对返回按钮的控制，否则将可能导致用户手动退出了App而广告无法正常曝光和计费 */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_HOME) {
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void navigateToNext() {
+        user = getUserFromCache();
+        if (user != null) {
+            showLoadingDialog();
+
+            student = user.getStudent();
+            MainActivity.start(this);
+            finish();
+        } else {
+            ViewGT.gotoLoginActivity(this);
+        }
     }
 }
